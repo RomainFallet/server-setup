0cea9d6f3657c90cd25cc62dbaf657170544f6c9
Romain Fallet <rfallet@numee.fr>
2020-08-22 13:24:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,230 @@
+# Server setup instructions
+
+The purpose of this repository is to provide instructions to set up a machine
+(SSH login, firewall, faill2ban, monitoring...) with (optionally) web hosting capabilities.
+
+The goal is to provide an opinionated environment that just work for commons scenarios.
+
+## Table of contents
+
+- [Prerequisites](#prerequisites)
+  - [Create a user account with sudo privileges](#create-a-user-account-with-sudo-privileges)
+  - [Configure an SSH key](#configure-an-ssh-key)
+  - [Point your domain names to your machine IP address](#point-your-domain-names-to-your-machine-ip-address)
+- [Quickstart](#quickstart)
+- [Transfer your files from your computer](#transfer-your-files-from-your-computer)
+- [Transfer your files from CI/CD](#transfer-your-files-from-cicd)
+
+## Prerequisites
+
+### Create a user account with sudo privileges
+
+[Back to top ↑](#table-of-contents)
+
+By default, if you install Ubuntu manually, it will ask you to create
+a user account with sudo privileges and disable root login automatically.
+This is how you are supposed to use your machine. This is because part of
+the power inherent with the root account is the ability to make very
+destructive changes, even by accident.
+
+But, in most cases, the Ubuntu install process is handled by your hosting
+provider which gives you directly access to the root account.
+If you are in this case, follow these steps:
+
+```bash
+# Login to your machine's root account
+ssh root@<ipAddress>
+
+# Create a new user
+adduser <username>
+
+# Grant sudo privileges to the newly created user
+usermod -aG sudo <username>
+
+# Disable root password
+passwd -l root
+
+# Disconnect
+exit
+```
+
+Sometimes, Ubuntu comes already preinstalled with a non-root sudo user named
+"ubuntu". In that case, you probably just want to rename it before using it:
+
+<!-- markdownlint-disable MD013 -->
+```bash
+# Login to your machine's "ubuntu" account
+ssh ubuntu@<ipAddress>
+
+# Define a password for the root account
+sudo passwd root
+
+# Allow root login with password through SSH
+sudo sed -i'.backup' -e 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+# Restart SSH
+sudo service ssh restart
+
+# Disconnect from your machine
+exit
+
+# Login to your machine's root account
+ssh root@<ipAddress>
+
+# Rename user
+usermod -l <newUserName> ubuntu
+
+# Rename home directory
+usermod -d /home/<newUserName> -m <newUserName>
+
+# Change password
+passwd <newUserName>
+
+# Disable root password
+sudo passwd -l root
+
+# Disallow root login with password through SSH
+sudo sed -i'.backup' -e 's/PermitRootLogin yes/#PermitRootLogin prohibit-password/g' /etc/ssh/sshd_config
+
+# Restart SSH
+sudo service ssh restart
+
+# Disconnect from your machine
+exit
+```
+<!-- markdownlint-enable MD013 -->
+
+_SSH client is enabled by default on Windows since the 2018 April update (1804).
+Download the update if you have an error when using SSH command in PowerShell._
+
+### Configure an SSH key
+
+[Back to top ↑](#table-of-contents)
+
+Before going any further, you need to generate an SSH key
+and add it to your server machine.
+
+```bash
+ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/id_rsa
+```
+
+Then add it to your machine by using:
+
+<!-- markdownlint-disable MD013 -->
+```bash
+ssh <yourUserName>@<yourIpAddress> "echo '$(cat ~/.ssh/id_rsa.pub)' | tee -a ~/.ssh/authorized_keys > /dev/null"
+```
+<!-- markdownlint-enable MD013 -->
+
+### Point your domain names to your machine IP address
+
+[Back to top ↑](#table-of-contents)
+
+Before continuing, your machine needs to have a dedicated domain name
+that will be its hostname.
+
+You also need to point your app domain names to your machine IP address
+if you want to host an app for them. See with your domain name registrar
+to set up A (IPV4) or AAAA (IPV6) records to perform this operation.
+
+A minimal DNS zone typically looks like this:
+
+![minimal-dns-zone](https://user-images.githubusercontent.com/6952638/84637979-ae703b00-aef6-11ea-8343-0f2036609a6c.png)
+
+For example, after that, you will be able to login with:
+
+```bash
+ssh <username>@mymachine.example.com
+```
+
+Instead of:
+
+```bash
+ssh <username>@50.70.150.30
+```
+
+## Quickstart
+
+[Back to top ↑](#table-of-contents)
+
+Login to your machine's sudo user and run the following commands.
+
+### Installation
+
+```bash
+git clone https://github/com/RomainFallet/server-setup
+```
+
+### Server setup
+
+```bash
+bash -c "$(cat ~/server-setup/server.sh)"
+```
+
+This will install all softwares needed to host production apps.
+
+### Configure a new app
+
+```bash
+bash -c "$(cat ~/server-setup/app.sh)"
+```
+
+After that, you will be able to login to your app with:
+
+```bash
+ssh -p 3022 <appname>@<hostname>
+```
+
+If you need to deploy your app through CI & CD, follow [these instructions](#transfer-your-files-from-cicd).
+
+### Transfer your files from your computer
+
+[Back to top ↑](#table-of-contents)
+
+All you need to do now is to transfer your app files using the SSH user created
+for your app (in the home directory) through SFTP or SSH.
+
+You can use the [Filezilla FTP client](https://filezilla-project.org/) or automate
+the process using a tool like [Rsync](https://linux.die.net/man/1/rsync).
+
+Don’t forget to only install production dependencies and to configure environment
+variables in the `.env.local` file (if you're using DotENV)
+before transferring your files.
+
+### Transfer your files from CI/CD
+
+[Back to top ↑](#table-of-contents)
+
+Using our private key directly on a machine owned by a Continuous Integration
+& Continuous Delivery (CI & CD) service provider (such as GitHub Actions,
+GitLab, Jenkins, Travis, etc.) is **INSECURED**.
+
+All your machines probably accept this private key, because, well, you are the admin.
+
+You don't know how the CI & CD services will store your key (even if they claimed
+they are secured). The risk is that if your private key is compromised and stolen,
+all your machines will be opened to the thief.
+
+Instead, we will create a new SSH private/public key pair that will be
+**dedicated to the CI & CD usage of this app**. If it's compromised, only this
+app can be damaged and you can easily revoke it from the production server
+without removing your own access.
+
+From your local computer, create new SSH keys for you app (replace "appname"):
+
+```bash
+ssh-keygen -t rsa -b 4096 -N '' -f ~/.ssh/<appname>.id_rsa -C '<appname>'
+```
+
+Then, **backup the content of these files** (in a password manager app for example).
+
+You can add the key to the SSH user of the app with:
+
+<!-- markdownlint-disable MD013 -->
+```bash
+ssh -p 3022 -t <adminusername>@<hostname> "echo '$(cat ~/.ssh/<appname>.id_rsa.pub)' | sudo tee -a /home/<appname>/.ssh/authorized_keys"
+```
+<!-- markdownlint-enable MD013 -->
+
+You can then safely copy this private key to the CI & CD service
+provider of your choice.
---
+++ server.sh
@@ -0,0 +1,485 @@
+#!/bin/bash
+
+# Exit script on error
+set -e
+
+### Set up variables
+
+# Ask hostname if not already set
+if [[ -z "${hostname}" ]]
+then
+  read -r -p "Enter your hostname (it must be a domain name pointing to this machine IP address): " hostname
+fi
+
+# Ask for monitoring email
+if [[ -z "${monitoringemails}" ]]
+then
+  read -r -p "Do you want to receive monitoring emails from this machine? [Y/n]: " monitoringemails
+  remotesmtp=${monitoringemails:-y}
+  remotesmtp=$(echo "${monitoringemails}" | awk '{print tolower($0)}')
+fi
+
+if [[ "${monitoringemails}" == 'y' ]]
+then
+  # Ask email if not already set
+  if [[ -z "${email}" ]]
+  then
+    read -r -p "Enter your email: " email
+  fi
+
+
+  # Ask for remote SMTP
+  if [[ -z "${remotesmtp}" ]]
+  then
+    read -r -p "Send monitoring emails from a remote SMTP server instead of this machine? (recommended) [Y/n]: " remotesmtp
+    remotesmtp=${remotesmtp:-y}
+    remotesmtp=$(echo "${remotesmtp}" | awk '{print tolower($0)}')
+  fi
+
+  if [[ "${remotesmtp}" == 'y' ]]
+  then
+    # Ask SMTP hostname if not already set
+    if [[ -z "${smtphostname}" ]]
+    then
+    read -r -p "Enter your remote SMTP server hostname: " smtphostname
+    fi
+
+    # Ask SMTP port if not already set
+    if [[ -z "${smtpport}" ]]
+    then
+    read -r -p "Enter your remote SMTP server port: " smtpport
+    fi
+
+    # Ask SMTP username if not already set
+    if [[ -z "${smtpusername}" ]]
+    then
+    read -r -p "Enter your remote SMTP server username: " smtpusername
+    fi
+
+    # Ask SMTP username if not already set
+    if [[ -z "${smtppassword}" ]]
+    then
+    read -r -p "Enter your SMTP password: " smtppassword
+    fi
+  fi
+fi
+
+### Timezone
+
+# Change timezone
+sudo timedatectl set-timezone Europe/Paris
+
+### Hostname
+
+# Change hostname
+sudo hostnamectl set-hostname "${hostname}"
+
+### SSH
+
+# Change default port
+sshportconfig='Port 3022'
+if ! sudo grep "^${sshportconfig}" /etc/ssh/sshd_config > /dev/null
+then
+  sudo sed -i'.backup' -E "s/#*Port\s+[0-9]+/${sshportconfig}/g" /etc/ssh/sshd_config
+fi
+if ! sudo grep "^${sshportconfig}" /etc/ssh/sshd_config > /dev/null
+then
+  echo "${sshportconfig}" | sudo tee -a /etc/ssh/sshd_config > /dev/null
+fi
+
+# Disable password authentication
+sshpassconfig='PasswordAuthentication no'
+sudo sed -i'.backup' -E "s/#*PasswordAuthentication\s+(\w+)/PasswordAuthentication no/g" /etc/ssh/sshd_config
+if ! sudo grep "^${sshpassconfig}" /etc/ssh/sshd_config > /dev/null
+then
+  echo "${sshpassconfig}" | sudo tee -a /etc/ssh/sshd_config > /dev/null
+fi
+
+# Keep alive client connections
+sshclientintervalconfig='ClientAliveInterval 120'
+sudo sed -i'.backup' -E "s/#*ClientAliveInterval\s+([0-9]+)/ClientAliveInterval 120/g" /etc/ssh/sshd_config
+if ! sudo grep "^${sshclientintervalconfig}" /etc/ssh/sshd_config > /dev/null
+then
+  echo "${sshclientintervalconfig}" | sudo tee -a /etc/ssh/sshd_config > /dev/null
+fi
+
+sshclientcountconfig='ClientAliveCountMax 3'
+sudo sed -i'.backup' -E "s/#*ClientAliveCountMax\s+([0-9]+)/ClientAliveCountMax 3/g" /etc/ssh/sshd_config
+if ! sudo grep "^${sshclientcountconfig}" /etc/ssh/sshd_config > /dev/null
+then
+  echo "${sshclientcountconfig}" | sudo tee -a /etc/ssh/sshd_config > /dev/null
+fi
+
+# Restart SSH
+sudo service ssh restart
+
+### Updates
+
+# Install latest updates
+sudo apt update && sudo apt dist-upgrade -y
+
+# Make a backup of the config files
+sudo cp /etc/apt/apt.conf.d/10periodic /etc/apt/apt.conf.d/.10periodic.backup
+sudo cp /etc/apt/apt.conf.d/50unattended-upgrades /etc/apt/apt.conf.d/.50unattended-upgrades.backup
+
+# Download upgradable packages automatically
+echo "APT::Periodic::Update-Package-Lists \"1\";
+APT::Periodic::Download-Upgradeable-Packages \"1\";
+APT::Periodic::AutocleanInterval \"7\";" | sudo tee /etc/apt/apt.conf.d/10periodic > /dev/null
+
+# Install updates automatically
+updateconfig="Unattended-Upgrade::Allowed-Origins {
+  \"\${distro_id}:\${distro_codename}\";
+  \"\${distro_id}:\${distro_codename}-security\";
+  \"\${distro_id}ESMApps:\${distro_codename}-apps-security\";
+  \"\${distro_id}ESM:\${distro_codename}-infra-security\";
+  \"\${distro_id}:\${distro_codename}-updates\";
+};
+Unattended-Upgrade::DevRelease \"false\";
+Unattended-Upgrade::Remove-Unused-Kernel-Packages \"true\";
+Unattended-Upgrade::Remove-Unused-Dependencies \"true\";
+Unattended-Upgrade::Automatic-Reboot \"true\";
+Unattended-Upgrade::Automatic-Reboot-Time \"05:00\";"
+if [[ "${monitoringemails}" == 'y' ]]
+then
+updateconfig+="
+Unattended-Upgrade::Mail \"${email}\";
+Unattended-Upgrade::MailOnlyOnError \"true\";"
+fi
+echo "${updateconfig}" | sudo tee /etc/apt/apt.conf.d/50unattended-upgrades > /dev/null
+
+### Default umask
+
+# Change default system umask
+sudo sed -i'.backup' -E 's/UMASK(\s+)([0-9]+)/UMASK\1002/g' /etc/login.defs
+
+### Postfix (optional)
+
+if [[ "${monitoringemails}" == 'y' && "${remotesmtp}" == 'y' ]]
+then
+  # Install
+  sudo DEBIAN_FRONTEND=noninteractive apt install -y postfix mailutils
+
+  # Make a backup of the config files
+  sudo cp /etc/postfix/main.cf /etc/postfix/.main.cf.backup
+  sudo cp /etc/aliases /etc/.aliases.backup
+
+  # Update main config file
+  echo "# See /usr/share/postfix/main.cf.dist for a commented, more complete version
+# Debian specific:  Specifying a file name will cause the first
+# line of that file to be used as the name.  The Debian default
+# is /etc/mailname.
+#myorigin = /etc/mailname
+
+smtpd_banner = \$myhostname ESMTP \$mail_name (Ubuntu)
+biff = no
+
+# appending .domain is the MUA's job.
+append_dot_mydomain = no
+
+# Uncomment the next line to generate \"delayed mail\" warnings
+#delay_warning_time = 4h
+
+readme_directory = no
+
+# See http://www.postfix.org/COMPATIBILITY_README.html -- default to 2 on
+# fresh installs.
+compatibility_level = 2
+
+# TLS parameters
+smtpd_tls_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem
+smtpd_tls_key_file=/etc/ssl/private/ssl-cert-snakeoil.key
+smtpd_use_tls=yes
+smtpd_tls_session_cache_database = btree:\${data_directory}/smtpd_scache
+smtp_tls_session_cache_database = btree:\${data_directory}/smtp_scache
+
+# See /usr/share/doc/postfix/TLS_README.gz in the postfix-doc package for
+# information on enabling SSL in the smtp client.
+
+smtpd_relay_restrictions = permit_mynetworks permit_sasl_authenticated defer_unauth_destination
+myhostname = ${hostname}
+relayhost = [${smtphostname}]:${smtpport}
+alias_maps = hash:/etc/aliases
+alias_database = hash:/etc/aliases
+mailbox_size_limit = 0
+recipient_delimiter = +
+inet_interfaces = all
+inet_protocols = ipv4
+smtp_sasl_auth_enable = yes
+smtp_sasl_security_options = noanonymous
+smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
+smtp_use_tls = yes
+smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt
+sender_canonical_classes = envelope_sender, header_sender
+sender_canonical_maps =  regexp:/etc/postfix/sender_canonical_maps
+smtp_header_checks = regexp:/etc/postfix/header_check" | sudo tee /etc/postfix/main.cf > /dev/null
+
+  # Save SMTP credentials
+  echo "[${smtphostname}]:${smtpport} ${smtpusername}:${smtppassword}" | sudo tee /etc/postfix/sasl_passwd > /dev/null
+  sudo postmap /etc/postfix/sasl_passwd
+  sudo chown root:root /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
+  sudo chmod 0600 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
+
+  # Remap sender address
+  echo "/.+/    ${smtpusername}" | sudo tee /etc/postfix/sender_canonical_maps > /dev/null
+  echo "/From:.*/ REPLACE From: ${smtpusername}" | sudo tee /etc/postfix/header_check > /dev/null
+  sudo postmap /etc/postfix/sender_canonical_maps
+  sudo postmap /etc/postfix/header_check
+
+  # Forwarding System Mail to your email address
+  echo "root:     ${email}" | sudo tee -a /etc/aliases > /dev/null
+
+  # Enable aliases
+  sudo newaliases
+
+  # Restart Postfix
+  sudo service postfix restart
+
+  # Display Postfix version
+  postconf mail_version
+
+  echo "Email monitoring is enabled for your machine: ${hostname}." | mail -s "Email monitoring is enabled." "${email}"
+fi
+
+### Apache web server & Cerbot (optional)
+
+# Ask for apache
+if [[ -z "${apache}" ]]
+then
+  read -r -p "Do you want to install Apache webserver & Cerbot? [N/y]: " apache
+  apache=${apache:-n}
+  apache=$(echo "${apache}" | awk '{print tolower($0)}')
+fi
+
+if [[ "${apache}" == 'y' ]]
+then
+  # Install
+  sudo apt install -y apache2
+
+  # Enable modules
+  sudo a2enmod ssl
+  sudo a2enmod rewrite
+  sudo a2enmod proxy
+  sudo a2enmod proxy_http
+  sudo a2enmod headers
+
+  # Set umask of the Apache user
+  umaskconfig='umask 002'
+  if ! sudo grep "^${umaskconfig}" /etc/apache2/envvars > /dev/null
+  then
+    echo "${umaskconfig}" | sudo tee -a /etc/apache2/envvars > /dev/null
+  fi
+
+  # Disable default site
+  sudo a2dissite 000-default.conf
+
+  # Restart Apache
+  sudo service apache2 restart
+
+  apache2 -v
+  sudo apache2ctl -M
+
+  ### Certbot
+
+  # Add Certbot official repositories
+  sudo add-apt-repository universe
+  sudo add-apt-repository -y ppa:certbot/certbot
+
+  # Install
+  sudo apt install -y certbot
+
+  # Check certificates renewal every month
+  certbotrenewconfig='0 0    1 * *    root    certbot renew'
+  if ! sudo grep "^${certbotrenewconfig}" /etc/crontab > /dev/null
+  then
+    echo "${certbotrenewconfig}" | sudo tee -a /etc/crontab > /dev/null
+  fi
+
+  certbot --version
+fi
+
+### Firewall
+
+# Add rules and activate firewall
+sudo ufw allow 3022
+sudo ufw allow Postfix
+"${apache}" == 'y' && sudo ufw allow in "Apache Full"
+echo 'y' | sudo ufw enable
+
+sudo ufw status
+
+### Fail2ban
+
+# Install
+sudo apt install -y fail2ban
+
+# Add default configuration
+fail2banconfig="[DEFAULT]
+findtime = 3600
+bantime = 86400"
+if [[ "${monitoringemails}" == 'y' ]]; then
+  fail2banconfig+="
+destemail = ${email}
+action = %(action_mwl)s"
+fi
+fail2banconfig+="
+[sshd]
+enabled = true
+port = ssh
+filter = sshd
+logpath = /var/log/auth.log
+maxretry = 3
+
+[postfix]
+enabled  = true
+port     = smtp
+filter   = postfix
+logpath  = /var/log/mail.log
+maxretry = 5
+"
+
+if [[ "${apache}" == 'y' ]]
+then
+  fail2banconfig+="
+[apache]
+enabled  = true
+port     = http,https
+filter   = apache-auth
+logpath  = /var/log/apache*/*error.log
+maxretry = 6
+
+[apache-noscript]
+enabled  = true
+port     = http,https
+filter   = apache-noscript
+logpath  = /var/log/apache*/*error.log
+maxretry = 6
+
+[apache-overflows]
+enabled  = true
+port     = http,https
+filter   = apache-overflows
+logpath  = /var/log/apache*/*error.log
+maxretry = 2
+
+[apache-nohome]
+enabled  = true
+port     = http,https
+filter   = apache-nohome
+logpath  = /var/log/apache*/*error.log
+maxretry = 2
+
+[apache-botsearch]
+enabled  = true
+port     = http,https
+filter   = apache-botsearch
+logpath  = /var/log/apache*/*error.log
+maxretry = 2
+
+[apache-shellshock]
+enabled  = true
+port     = http,https
+filter   = apache-shellshock
+logpath  = /var/log/apache*/*error.log
+maxretry = 2
+
+[apache-fakegooglebot]
+enabled  = true
+port     = http,https
+filter   = apache-fakegooglebot
+logpath  = /var/log/apache*/*error.log
+maxretry = 2
+
+[php-url-fopen]
+enabled = true
+port    = http,https
+filter  = php-url-fopen
+logpath = /var/log/apache*/*access.log"
+fi
+
+echo "${fail2banconfig}" | sudo tee /etc/fail2ban/jail.local > /dev/null
+
+# Restart Fail2ban
+sudo service fail2ban restart
+
+fail2ban-client -V
+
+### PHP environment (optional)
+
+# Ask for PHP/ environment
+if [[ -z "${php}" ]]; then
+  read -r -p "Do you want to install PHP environment? [N/y]: " php
+  php=${php:-n}
+  php=$(echo "${php}" | awk '{print tolower($0)}')
+fi
+
+if [[ "${php}" == 'y' ]]; then
+  # Add PHP official repository
+  sudo add-apt-repository -y ppa:ondrej/php
+
+  # Install PHP
+  sudo apt install -y php7.3
+
+  # Install Redis for PHP cache
+  sudo apt install -y redis-server
+
+  # Install extensions
+  sudo apt install -y php7.3-mbstring php7.3-mysql php7.3-xml php7.3-curl php7.3-zip php7.3-intl php7.3-gd php-redis
+
+  # Make a backup of the config file
+  phpinipath=$(php -r "echo php_ini_loaded_file();")
+  sudo cp "${phpinipath}" "$(dirname "${phpinipath}")/.php.ini.backup"
+
+  # Update some configuration in php.ini
+  sudo sed -i'.tmp' -E 's/;*\s*post_max_size\s=\s*[0-8]+M/post_max_size = 64M/g' "${phpinipath}"
+  sudo sed -i'.tmp' -E 's/;*\s*upload_max_filesize\s=\s*[0-8]+M/upload_max_filesize = 64M/g' "${phpinipath}"
+  sudo sed -i'.tmp' -E 's/;*\s*memory_limit\s=\s*-*[0-8]+M*/memory_limit = 512M/g' "${phpinipath}"
+
+  # Disable functions that can causes security breaches
+  sudo sed -i'.tmp' -E 's/;*\s*disable_functions\s=\s*(\w+)/disable_functions = error_reporting,ini_set,exec,passthru,shell_exec,system,proc_open,popen,parse_ini_file,show_source/g' "${phpinipath}"
+
+  # Replace default PHP installation in $PATH
+  sudo update-alternatives --set php /usr/bin/php7.3
+
+  # Remove temporary file
+  sudo rm "${phpinipath}.tmp"
+
+  # Apply PHP configuration to Apache
+  if [[ "${apache}" == 'y' ]];
+  then
+    sudo cp /etc/php/7.3/apache2/php.ini /etc/php/7.3/apache2/.php.ini.backup
+    sudo cp "${phpinipath}" /etc/php/7.3/apache2/php.ini
+  fi
+
+  # Add MariaDB official repository
+  test -f /etc/apt/sources.list.d/mariadb.list || curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo -E bash -s -- --mariadb-server-version=mariadb-10.4
+
+  # Install
+  sudo apt install -y mariadb-server-10.4
+fi
+
+### NodeJS environment (optional)
+
+# Ask for NodeJS environment
+if [[ -z "${nodejs}" ]]; then
+  read -r -p "Do you want to install NodeJS environment? [N/y]: " nodejs
+  nodejs=${nodejs:-n}
+  nodejs=$(echo "${nodejs}" | awk '{print tolower($0)}')
+fi
+
+if [[ "${nodejs}" == 'y' ]]; then
+  # Add NodeJS official repository and update packages list
+  curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+  # Install
+  sudo apt install -y nodejs
+
+  # Install PM2 process manager
+  sudo npm install -g pm2@4.4.0
+
+  # Add MariaDB official repository
+  test -f /etc/apt/sources.list.d/mariadb.list || curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo -E bash -s -- --mariadb-server-version=mariadb-10.4
+
+  # Install
+  sudo apt install -y mariadb-server-10.4
+fi
