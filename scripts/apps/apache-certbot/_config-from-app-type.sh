b5708ab146235421561eca6b065f1e2130f26fcf
Romain Fallet <romain@fallet.net>
2020-12-29 10:50:15 +0100

    :art: Adjust indentation.

--- scripts/apps/apache-certbot/_config-from-app-type.sh
+++ scripts/apps/apache-certbot/_config-from-app-type.sh
@@ -38,32 +38,32 @@ then
   fi
 
   apacheconfigfromapptype="ProxyPass / http://127.0.0.1:${proxyport}/
-RewriteEngine On
-RewriteRule ^/\.well-known/carddav https://%{SERVER_NAME}/remote.php/dav/ [R=301,L]
-RewriteRule ^/\.well-known/caldav https://%{SERVER_NAME}/remote.php/dav/ [R=301,L]"
+  RewriteEngine On
+  RewriteRule ^/\.well-known/carddav https://%{SERVER_NAME}/remote.php/dav/ [R=301,L]
+  RewriteRule ^/\.well-known/caldav https://%{SERVER_NAME}/remote.php/dav/ [R=301,L]"
 fi
 
 if [[ "${apptype}" == '3' ]]
 then
   apacheconfigfromapptype="DocumentRoot /var/www/${appname}
-<Directory /var/www/${appname}>
-  Require all granted
-  Options -Indexes -FollowSymLinks
-  AllowOverride None
-</Directory>"
+  <Directory /var/www/${appname}>
+    Require all granted
+    Options -Indexes -FollowSymLinks
+    AllowOverride None
+  </Directory>"
 fi
 
 if [[ "${apptype}" == '4' ]]
 then
   apacheconfigfromapptype="DocumentRoot /var/www/${appname}
-<Directory /var/www/${appname}>
-  Require all granted
-  Options -Indexes -FollowSymLinks
-  AllowOverride None
-  RewriteEngine on
-  RewriteCond %{REQUEST_FILENAME} !-f
-  RewriteRule ^ index.html [QSA,L]
-</Directory>"
+  <Directory /var/www/${appname}>
+    Require all granted
+    Options -Indexes -FollowSymLinks
+    AllowOverride None
+    RewriteEngine on
+    RewriteCond %{REQUEST_FILENAME} !-f
+    RewriteRule ^ index.html [QSA,L]
+  </Directory>"
 fi
 
 if [[ "${apptype}" == '5' ]]
@@ -73,37 +73,37 @@ then
     sudo mkdir "/var/www/${appname}/tmp"
   fi
   apacheconfigfromapptype="DocumentRoot /var/www/${appname}
-<Directory /var/www/${appname}/public>
-  Require all granted
-  Options -Indexes -FollowSymLinks
-  AllowOverride None
-  php_admin_value open_basedir '/var/www/${appname}'
-  php_admin_value upload_tmp_dir '/var/www/${appname}/tmp'
-  FallbackResource /index.php
-</Directory>
-<Directory /var/www/${appname}/public/bundles>
-  FallbackResource disabled
-</Directory>
-<FilesMatch \.php$>
-  SetHandler 'proxy:unix:/run/php/php7.3-fpm.sock|fcgi://localhost'
-</FilesMatch>"
+  <Directory /var/www/${appname}/public>
+    Require all granted
+    Options -Indexes -FollowSymLinks
+    AllowOverride None
+    php_admin_value open_basedir '/var/www/${appname}'
+    php_admin_value upload_tmp_dir '/var/www/${appname}/tmp'
+    FallbackResource /index.php
+  </Directory>
+  <Directory /var/www/${appname}/public/bundles>
+    FallbackResource disabled
+  </Directory>
+  <FilesMatch \.php$>
+    SetHandler 'proxy:unix:/run/php/php7.3-fpm.sock|fcgi://localhost'
+  </FilesMatch>"
 fi
 
 if [[ "${apptype}" == '6' ]]
 then
   # shellcheck disable=SC2034
   apacheconfigfromapptype="DocumentRoot /var/www/${appname}
-<Directory /var/www/${appname}>
-  Require all granted
-  Options +FollowSymLinks +MultiViews
-  AllowOverride All
-  php_admin_value open_basedir '/var/www/${appname}'
-  php_admin_value upload_tmp_dir '/var/www/${appname}/tmp'
-</Directory>
-<IfModule mod_dav.c>
-  Dav off
-</IfModule>
-<FilesMatch \.php$>
-  SetHandler 'proxy:unix:/run/php/php7.3-fpm.sock|fcgi://localhost'
-</FilesMatch>"
+  <Directory /var/www/${appname}>
+    Require all granted
+    Options +FollowSymLinks +MultiViews
+    AllowOverride All
+    php_admin_value open_basedir '/var/www/${appname}'
+    php_admin_value upload_tmp_dir '/var/www/${appname}/tmp'
+  </Directory>
+  <IfModule mod_dav.c>
+    Dav off
+  </IfModule>
+  <FilesMatch \.php$>
+    SetHandler 'proxy:unix:/run/php/php7.3-fpm.sock|fcgi://localhost'
+  </FilesMatch>"
 fi
