8ef5fe45e15ceef05ecb209a9a6c0e0a2e72e5c7
Romain Fallet <romain@fallet.net>
2021-02-27 21:53:10 +0100

    fixup! feat: add mariadb 10.5 setup

---
+++ scripts/server/databases/mariadb/mariadb-10-4.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+set -e
+
+### MariaDB 10.4
+
+# Add repository key
+if ! sudo apt-key list | grep 'MariaDB'
+then
+  sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+fi
+
+# Add repository
+if ! grep 'MariaDB/repo/10.4' /etc/apt/sources.list
+then
+  sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mariadb.mirrors.ovh.net/MariaDB/repo/10.4/ubuntu focal main'
+  sudo apt update
+fi
+
+# Install
+sudo apt install -y mariadb-server-10.4
+
+# Stop service
+sudo service mariadb stop
+sudo rm -f /lib/systemd/system/mariadb.service
+sudo systemctl daemon-reload
+
+# Create config directory
+if ! test -d /etc/mariadb
+then
+  sudo mkdir /etc/mariadb
+fi
+if ! test -d /etc/mariadb/10.4
+then
+  sudo mv /etc/mysql /etc/mariadb/10.4
+  sudo rm /etc/mariadb/10.4/my.cnf
+  echo "[client-server]
+  socket=/var/run/mariadb/10.4/mariadb.sock
+  port=3308
+  [mysqld]
+  user=mysql
+  datadir=/var/lib/mariadb/10.4
+  log_error=/var/log/mariadb/10.4/error.log" | sudo tee /etc/mariadb/10.4/my.cnf > /dev/null
+fi
+
+# Create log directory
+if ! test -d /var/log/mariadb/10.4
+then
+  sudo mkdir -p /var/log/mariadb/10.4
+fi
+
+# Create data directory
+if ! test -d /var/lib/mariadb
+then
+  sudo mkdir /var/lib/mariadb
+fi
+if ! test -d /var/lib/mariadb/10.4
+then
+  sudo mv /var/lib/mysql /var/lib/mariadb/10.4
+fi
+
+# Create socket directory
+if ! test -d /var/run/mariadb
+then
+  sudo mkdir /var/run/mariadb
+fi
+if ! test -d /var/run/mariadb/10.4/
+then
+  sudo mv /var/run/mysqld /var/run/mariadb/10.4/
+fi
+
+# Create bin directory
+if ! test -d /usr/local/mariadb/10.4/bin
+then
+  sudo mkdir -p /usr/local/mariadb/10.4/bin
+  sudo mv /usr/bin/mariadb* /usr/local/mariadb/10.4/bin/
+fi
+
+# Create sbin directory
+if ! test -d /usr/local/mariadb/10.4/sbin
+then
+  sudo mkdir -p /usr/local/mariadb/10.4/sbin
+  sudo mv /usr/sbin/mariadb* /usr/local/mariadb/10.4/sbin/
+fi
+
+# Clean up installation
+sudo rm -rf /usr/bin/mysql* /usr/sbin/mysql* /var/lib/mariadb /etc/mysql /var/run/mysqld /var/log/mysql
+sudo apt autoremove --purge -y mariadb-server-10.4
+
+# Create service file
+if ! test -f /lib/systemd/system/mariadb-10.4.service
+then
+  echo "[Unit]
+  Description=Mariadb 10.4
+
+  Wants=network.target
+  After=syslog.target network-online.target
+
+  [Service]
+  Type=simple
+  ExecStart=/usr/local/mariadb/10.4/sbin/mariadbd --defaults-file=/etc/mariadb/10.4/my.cnf
+  Restart=on-failure
+  RestartSec=10
+  KillMode=mixed
+
+  [Install]
+  WantedBy=multi-user.target" | sudo tee /lib/systemd/system/mariadb-10.4.service > /dev/null
+  sudo systemctl daemon-reload
+fi
+
+# Enable service on system startup
+sudo systemctl enable mariadb-10.4
+sudo service mariadb-10.4 start
--- scripts/server/databases/mariadb/mariadb-10-5.sh
+++ scripts/server/databases/mariadb/mariadb-10-5.sh
@@ -3,21 +3,29 @@
 set -e
 
 ### MariaDB 10.5
+
+# Add repository key
 if ! sudo apt-key list | grep 'MariaDB'
 then
   sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
 fi
+
+# Add repository
 if ! grep 'MariaDB/repo/10.5' /etc/apt/sources.list
 then
   sudo add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://mariadb.mirrors.ovh.net/MariaDB/repo/10.5/ubuntu focal main'
   sudo apt update
 fi
 
+# Install
 sudo apt install -y mariadb-server-10.5
+
+# Stop service
 sudo service mariadb stop
 sudo rm -f /lib/systemd/system/mariadb.service
 sudo systemctl daemon-reload
 
+# Create config directory
 if ! test -d /etc/mariadb
 then
   sudo mkdir /etc/mariadb
@@ -27,17 +35,21 @@ then
   sudo mv /etc/mysql /etc/mariadb/10.5
   sudo rm /etc/mariadb/10.5/my.cnf
   echo "[client-server]
-  socket=/tmp/mariadb-10.5.sock
+  socket=/var/run/mariadb/10.5/mariadb.sock
   port=3307
   [mysqld]
   user=mysql
   datadir=/var/lib/mariadb/10.5
-  log_error=/var/log/mysql/mariadb.err" | sudo tee /etc/mariadb/10.5/my.cnf > /dev/null
+  log_error=/var/log/mariadb/10.5/error.log" | sudo tee /etc/mariadb/10.5/my.cnf > /dev/null
 fi
+
+# Create log directory
 if ! test -d /var/log/mariadb/10.5
 then
   sudo mkdir -p /var/log/mariadb/10.5
 fi
+
+# Create data directory
 if ! test -d /var/lib/mariadb
 then
   sudo mkdir /var/lib/mariadb
@@ -46,6 +58,8 @@ if ! test -d /var/lib/mariadb/10.5
 then
   sudo mv /var/lib/mysql /var/lib/mariadb/10.5
 fi
+
+# Create socket directory
 if ! test -d /var/run/mariadb
 then
   sudo mkdir /var/run/mariadb
@@ -54,20 +68,26 @@ if ! test -d /var/run/mariadb/10.5/
 then
   sudo mv /var/run/mysqld /var/run/mariadb/10.5/
 fi
+
+# Create bin directory
 if ! test -d /usr/local/mariadb/10.5/bin
 then
   sudo mkdir -p /usr/local/mariadb/10.5/bin
   sudo mv /usr/bin/mariadb* /usr/local/mariadb/10.5/bin/
 fi
+
+# Create sbin directory
 if ! test -d /usr/local/mariadb/10.5/sbin
 then
   sudo mkdir -p /usr/local/mariadb/10.5/sbin
   sudo mv /usr/sbin/mariadb* /usr/local/mariadb/10.5/sbin/
 fi
 
+# Clean up installation
+sudo rm -rf /usr/bin/mysql* /usr/sbin/mysql* /var/lib/mariadb /etc/mysql /var/run/mysqld /var/log/mysql
 sudo apt autoremove --purge -y mariadb-server-10.5
-sudo rm -rf /usr/bin/mysql* /usr/sbin/mysql*
 
+# Create service file
 if ! test -f /lib/systemd/system/mariadb-10.5.service
 then
   echo "[Unit]
@@ -85,6 +105,9 @@ then
 
   [Install]
   WantedBy=multi-user.target" | sudo tee /lib/systemd/system/mariadb-10.5.service > /dev/null
+  sudo systemctl daemon-reload
 fi
-sudo systemctl daemon-reload
+
+# Enable service on system startup
+sudo systemctl enable mariadb-10.5
 sudo service mariadb-10.5 start
