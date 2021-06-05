dcd3d850f09f7bf3149bf646cde12ba0d01908f0
Romain Fallet <romain@fallet.net>
2021-06-05 12:21:40 +0200

    fix: fix mailserver recipe

--- scripts/recipes/mail-server.sh
+++ scripts/recipes/mail-server.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 # shellcheck source=../server/basic.sh
-source ~/server-setup/server/basic.sh
+source ~/server-setup/scripts/server/basic.sh
 
 # shellcheck source=../apps/mailinabox/0.53a/install.sh
-source ~/server-setup/apps/mailinabox/0.53a/install.sh
+source ~/server-setup/scripts/apps/mailinabox/0.53a/install.sh
 
 restorebackup=$1
 if [[ -z "${restorebackup}" ]]
@@ -17,11 +19,11 @@ fi
 if [[ "${restorebackup}" == 'y' ]]
 then
   # shellcheck source=../management/rsync/restore-backup.sh
-  source ~/server-setup/management/rsync/restore-backup.sh /home/user-data
+  source ~/server-setup/scripts/management/rsync/restore-backup.sh /home/user-data
 
   # shellcheck source=../apps/mailinabox/0.53a/install.sh
-  source ~/server-setup/apps/mailinabox/0.53a/install.sh
+  source ~/server-setup/scripts/apps/mailinabox/0.53a/install.sh
 fi
 
 # shellcheck source=../management/rsync/set-up-hourly-backup.sh
-source ~/server-setup/management/rsync/set-up-hourly-backup.sh /home/user-data
+source ~/server-setup/scripts/management/rsync/set-up-hourly-backup.sh /home/user-data
