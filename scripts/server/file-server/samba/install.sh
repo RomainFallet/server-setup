f30e87523f9230b6980efef8be8c8ec6e1ef3b4c
Romain Fallet <romain@fallet.net>
2021-07-29 10:32:11 +0200

    fixup! fixup! fixup! feat: add samba instructions

--- scripts/server/file-server/samba/install.sh
+++ scripts/server/file-server/samba/install.sh
@@ -6,7 +6,17 @@ set -e
 ### Samba file server
 
 # Install
-sudo apt install -y samba
+sudo apt install -y samba unzip
 
 # Allow Samba connections
 sudo ufw allow samba
+
+# Download and install Windows Network Discovery
+wget https://github.com/christgau/wsdd/archive/master.zip -P /tmp
+unzip /tmp/master.zip -d /tmp
+mv /tmp/wsdd-master/src/wsdd.py /tmp/wsdd-master/src/wsdd
+sudo cp /tmp/wsdd-master/src/wsdd /usr/bin
+sudo cp /tmp/wsdd-master/etc/systemd/wsdd.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl start wsdd
+sudo systemctl enable wsdd
