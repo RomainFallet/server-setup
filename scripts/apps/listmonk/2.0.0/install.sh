dd8d135ad2b8a0408d4adf2ea8cd60552104b361
Romain Fallet <romain@fallet.net>
2021-11-18 21:44:55 +0100

    fixup! feat: add listmont and postgresql

--- scripts/apps/listmonk/2.0.0/install.sh
+++ scripts/apps/listmonk/2.0.0/install.sh
@@ -3,61 +3,57 @@
 set -e
 
 #### Inputs
-databaseName=$1
-if [[ -z "${databaseName}" ]]
+appName=$1
+if [[ -z "${appName}" ]]
 then
-  read -r -p "Choose your database name: " databaseName
+  read -r -p "Choose your app name: " appName
 fi
 
-databaseUser=$2
-if [[ -z "${databaseUsername}" ]]
+appName=$2
+if [[ -z "${appPort}" ]]
 then
-  read -r -p "Choose your database username: " databaseUsername
+  read -r -p "Choose your app port: " appPort
 fi
 
-databasePassword=$3
+appDomain=$3
+if [[ -z "${appDomain}" ]]
+then
+  read -r -p "Choose your app domain: " appDomain
+fi
+
+adminUsername=$4
+if [[ -z "${adminUsername}" ]]
+then
+  read -r -p "Choose your app admin username: " adminUsername
+fi
+
+adminPassword=$5
+if [[ -z "${adminPassword}" ]]
+then
+  read -r -p "Choose your app admin password: " adminPassword
+fi
+
+databasePassword=$6
 if [[ -z "${databasePassword}" ]]
 then
   read -r -p "Choose your database password: " databasePassword
 fi
 
-listmonkDomain=$4
-if [[ -z "${listmonkDomain}" ]]
-then
-  read -r -p "Choose your Listmonk domain: " listmonkDomain
-fi
-
-adminUsername=$5
-if [[ -z "${adminUsername}" ]]
-then
-  read -r -p "Choose your Listmonk admin username: " adminUsername
-fi
-
-adminPassword=$6
-if [[ -z "${adminPassword}" ]]
-then
-  read -r -p "Choose your Listmonk admin password: " adminPassword
-fi
-
-email=$7
+email=$6
 if [[ -z "${email}" ]]
 then
   read -r -p "Enter your email (needed to request TLS certificate): " email
 fi
-
-#### Constants
-listmonkPort="9000"
-listmonkName="listmonk"
 
 #### Install dependencies
 bash ~/server-setup/scripts/server/postgresql/14/install.sh
 
 databasesList=$(sudo -u postgres psql -l)
 usersList=$(sudo -u postgres psql -c "\du+")
-echo "${databasesList}" | grep "${databaseName}" || sudo -u postgres psql -c "CREATE DATABASE '${databaseName}';"
-echo "${usersList}"  | grep "${databaseUsername}" || sudo -u postgres psql -d "${databaseName}" -c "CREATE USER ${databaseUsername};"
-sudo -u postgres psql -d "${databaseName}" -c "ALTER USER \"${databaseUsername}\" with encrypted password '${databasePassword}';"
-sudo -u postgres psql -d "${databaseName}" -c "GRANT ALL PRIVILEGES ON DATABASE \"${databaseName}\" TO \"${databaseUsername}\";"
+echo "${databasesList}" | grep "${appName}" || sudo -u postgres psql -c "CREATE DATABASE '${appName}';"
+echo "${usersList}"  | grep "${appName}" || sudo -u postgres psql -d "${appName}" -c "CREATE USER ${appName};"
+sudo -u postgres psql -d "${appName}" -c "ALTER USER \"${appName}\" with encrypted password '${databasePassword}';"
+sudo -u postgres psql -d "${appName}" -c "GRANT ALL PRIVILEGES ON DATABASE \"${appName}\" TO \"${appName}\";"
 
 #### Install app
 test -f /tmp/listmonk_2.0.0_linux_amd64.tar.gz || wget https://github.com/knadh/listmonk/releases/download/v2.0.0/listmonk_2.0.0_linux_amd64.tar.gz -P /tmp
@@ -67,16 +63,16 @@ rm /tmp/listmonk_2.0.0_linux_amd64.tar.gz
 #### Configure app
 listmonkConfig="
 [app]
-address = \"localhost:${listmonkPort}\"
+address = \"localhost:${appPort}\"
 admin_username = \"${adminUsername}\"
 admin_password = \"${adminPassword}\"
 
 [db]
 host = \"localhost\"
 port = 5432
-user = \"${databaseUser}\"
+user = \"${appName}\"
 password = \"${databasePassword}\"
-database = \"${databaseName}\"
+database = \"${appName}\"
 ssl_mode = \"disable\"
 max_open = 25
 max_idle = 25
@@ -85,11 +81,11 @@ max_lifetime = \"300s\"
 echo "${listmonkConfig}" | sudo tee /opt/listmonk/config.toml > /dev/null
 /opt/listmonk/listmonk --install --config /opt/listmonk/config.toml
 
-bash ~/server-setup/scripts/management/nginx-certbot/get-tls-certificate.sh "${listmonkName}" "${email}" "${listmonkDomain}"
-bash ~/server-setup/scripts/management/nginx-certbot/set-up-domain-name-app.sh "${listmonkName}" "${email}" "1" "${listmonkPort}"
+bash ~/server-setup/scripts/management/nginx-certbot/get-tls-certificate.sh "${appName}" "${email}" "${appDomain}"
+bash ~/server-setup/scripts/management/nginx-certbot/set-up-domain-name-app.sh "${appName}" "${email}" "1" "${appPort}"
 
 serviceConfig="[Unit]
-Description=Autostart tunnel service for ${listmonkName}
+Description=Autostart tunnel service for ${appName}
 After=network-online.target
 
 [Service]
@@ -100,8 +96,8 @@ ExecStart=//opt/listmonk/listmonk
 [Install]
 WantedBy=multi-user.target
 "
-echo "${serviceConfig}" | sudo tee "/etc/systemd/system/${listmonkName}.service" > /dev/null
+echo "${serviceConfig}" | sudo tee "/etc/systemd/system/${appName}.service" > /dev/null
 
 sudo systemctl daemon-reload
-sudo systemctl enable "${listmonkName}"
-sudo systemctl restart "${listmonkName}"
+sudo systemctl enable "${appName}"
+sudo systemctl restart "${appName}"
