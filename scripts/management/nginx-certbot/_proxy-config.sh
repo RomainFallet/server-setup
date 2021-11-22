0bd86ce2930ed4562f489e6ba766ea11f0cb5b52
Romain Fallet <romain@fallet.net>
2021-11-22 20:54:50 +0100

    fix: nginx proxy

--- scripts/management/nginx-certbot/_proxy-config.sh
+++ scripts/management/nginx-certbot/_proxy-config.sh
@@ -8,6 +8,8 @@ port=$2
 echo "root /var/www/${appname};
 
 location / {
+    proxy_set_header Host \$host;
+    proxy_set_header X-Real-IP \$remote_addr;
     proxy_pass http://localhost:${port};
 }
 "
