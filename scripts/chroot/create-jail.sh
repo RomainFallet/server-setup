22eccc8d8d07e6fee182d378d3e8404696f8f3c1
Romain Fallet <romain@fallet.net>
2021-12-12 20:47:15 +0100

    update

--- scripts/chroot/create-jail.sh
+++ scripts/chroot/create-jail.sh
@@ -25,31 +25,12 @@ sudo mkdir -p "${jailPath}/home/${username}"
 # Set permissions to home directory
 sudo chown "${username}:${username}" "${jailPath}/home/${username}"
 
-# Ask which type of jail we wants for the user
-# if [[ -z "${commandsList}" ]] && [[ -z "${useBasicCommands}" ]]; then
-#   read -r -p "Do you want your user to access only basic commands instead of all of them? [N/y]: " useBasicCommands
-#   useBasicCommands=${useBasicCommands:-n}
-#   useBasicCommands=$(echo "${useBasicCommands}" | awk '{print tolower($0)}')
-# fi
-
-# Ask for the commands list we want for the user
-# bash,ls,rm,touch,mkdir,rmdir
-# if [[ -z "${commandsList}" ]] && [[ "${useBasicCommands}" == 'y' ]]; then
-#   read -r -p "List basic commands (comma separated) you want to give access to: " commandsList
-#   if [[ -z "${commandsList}" ]]; then
-#     echo "You must supply some commands (at least \"bash\" to login)." 1>&2
-#     exit 1
-#   fi
-# fi
-commandsList="/bin/bash,/bin/ls,/bin/cp,/bin/mv,/bin/rm,/bin/touch,/bin/mkdir,/bin/rmdir,/usr/bin/vi"
-
-# Handle "basic commands access" case
-# if [[ -n "${commandsList}" ]]; then
-
+# Commands list to set up in the chroot jail
+commandsList="/bin/bash,/bin/ls,/bin/cp,/bin/mv,/bin/rm,/bin/touch,/bin/mkdir,/bin/rmdir,/usr/bin/vi,/usr/bin/rsync,/usr/bin/scp"
 IFS=',' read -ra commandsList <<< "${commandsList}"
 for commandPath in "${commandsList[@]}"
 do
-  # Deps
+  # Copy deps
   for depPath in $( (ldd "${commandPath}" || true) | (grep -v dynamic || true) | (cut -d " " -f 3 || true) | (sed 's/://' || true) | (sort || true) | uniq )
   do
     if [[ -f "${depPath}" ]] && [[ ! -f "${jailPath}${depPath}" ]]; then
@@ -57,13 +38,13 @@ do
     fi
   done
 
-  # Commands
+  # Copy commands
   if [[ -f "${commandPath}" ]] && [[ ! -f "${jailPath}${commandPath}" ]]; then
     sudo cp --parents "${commandPath}" "${jailPath}"
   fi
 done
 
-# Others deps
+# Copy others deps
 deps=( /lib64/ld-linux-x86-64.so.2 /lib/ld-linux.so.2 /lib/ld-linux-aarch64.so.1 )
 for depPath in "${deps[@]}"
 do
@@ -72,79 +53,12 @@ do
   fi
 done
 
-# Handle "full commands access" case
-# else
-#   # List of directories to mount in the jail
-#   directories=( /bin /etc /lib /lib64 /usr /dev /var/run /var/lib /tmp /run /proc /opt )
-
-#   for directory in "${directories[@]}"
-#   do
-#     # Create mount point
-#     echo "Creating mount point \"${jailPath}${directory}\"."
-#     if [[ ! -d "${jailPath}${directory}" ]]; then
-
-#       if ! sudo mkdir -p "${jailPath}${directory}"; then
-#         echo "Unable to create the mount point." 1>&2
-#         exit 1
-#       fi
-#       echo "Mount point created."
-#     else
-#       echo "Mount point already exists."
-#     fi
-
-#     # Set permissions to mount point
-#     if ! sudo chown root:root "${jailPath}${directory}" && sudo chmod 0755 "${jailPath}${directory}"; then
-#       echo "Unable to set appropriate permissions to the mount point." 1>&2
-#       exit 1
-#     fi
-#     echo "Set root:root as owner:group and 0755 permissions to the mount point."
-
-#     # Determine mount point read-write access
-#     directories_access=ro
-#     readwrite_directories=( /tmp )
-#     for readwrite_directory in "${readwrite_directories[@]}"
-#     do
-#       if [[ "${readwrite_directory}" == "${directory}" ]]; then
-#         directories_access=rw
-#       fi
-#     done
-#     echo "Mount point access is: ${directories_access}."
-
-#     # Bind mount
-#     echo "Mounting \"${directory}\" into \"${jailPath}${directory}\"."
-#     is_already_mounted=$(sudo grep "${jailPath}${directory}" /proc/mounts)
-
-#     if [[ -z "${is_already_mounted}" ]]; then
-#       if ! sudo mount --bind "${directory}" "${jailPath}${directory}" && sudo mount -o remount,${directories_access},bind "${jailPath}${directory}"; then
-#         echo "Unable to mount." 1>&2
-#         exit 1
-#       fi
-#       echo "Mounted."
-#     else
-#       echo "Already mounted."
-#     fi
-
-#     # Make the mount permanent
-#     echo "Making \"${jailPath}${directory}\" mount permanent in \"/etc/fstab\"."
-#     is_permanent=$(sudo grep "${jailPath}${directory}" /etc/fstab)
-
-#     if [[ -z "${is_permanent}" ]]; then
-#       mountconfig="${directory} ${jailPath}${directory} none ${directories_access},bind 0 0"
-#       if ! echo "${mountconfig}" | sudo tee -a /etc/fstab > /dev/null; then
-#         echo "Unable to apply permanently the mount config." 1>&2
-#         exit 1
-#       fi
-#       echo "Mount config applied permanently."
-#     else
-#       echo "Mount config already permanent."
-#     fi
-#   done
-# fi
-
 # Set up chroot directory
 sshConfig="
 Match User ${username}
-ChrootDirectory ${jailPath}
+  ChrootDirectory ${jailPath}
+  AllowTcpForwarding no
+  X11Forwarding no
 "
 sshConfigPath=/etc/ssh/sshd_config
 pattern=$(echo "${sshConfig}" | tr -d '\n')
@@ -152,6 +66,15 @@ content=$(< "${sshConfigPath}" tr -d '\n')
 if [[ "${content}" != *"${pattern}"* ]]
 then
   echo "${sshConfig}" | sudo tee -a "${sshConfigPath}" > /dev/null
+fi
+
+# Enable SFTP
+sshSftpConfig='Subsystem sftp internal-sftp'
+sudo sed -i'.tmp' -E "s/#*Subsystem sftp\s+(\w+)/${sshSftpConfig}/g" "${sshConfigPath}"
+sudo rm -f "${sshConfigPath}".tmp
+if ! sudo grep "^${sshSftpConfig}" "${sshConfigPath}" > /dev/null
+then
+  echo "${sshSftpConfig}" | sudo tee -a "${sshConfigPath}" > /dev/null
 fi
 
 # Set bash as default shell
