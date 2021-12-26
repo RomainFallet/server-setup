3349e1eec82c54a5144455aee7b10cf8a5ab7205
Romain Fallet <romain@fallet.net>
2021-12-26 22:06:00 +0100

    update

--- scripts/deluge/set-up-auto-add-service.sh
+++ scripts/deluge/set-up-auto-add-service.sh
@@ -22,15 +22,31 @@ autoAddServiceScript="#!/bin/bash
 # Exit script on error
 set -e
 
-inotifywait --monitor ${directoryPathToWatch} --recursive --event create --event moved_to |
+inotifywait --monitor ${directoryPathToWatch} --recursive --event create --event moved_to --event delete |
 while read -r row; do
   if [[ \"\${row}\" =~ .torrent$ ]]; then
-    delimiter=\$((echo \"\${row}\" | grep ' CREATE ' && echo ' CREATE ') || echo ' MOVED_TO ')
-    directoryPath=\$(echo \"\${row}\" | cut -d\"\${delimiter}\" -f1)
-    action=\$(echo \"\${row}\" | cut -d\"\${delimiter}\" -f2)
-    fileName=\$(echo \"\${row}\" | cut -d\"\${delimiter}\" -f3)
-    # deluge-console --daemon 127.0.0.1 --port 58846 --username deluge --password deluge \"add \${directoryPath}\${file} --path=\${directoryPath}; exit\"
-    echo \"['\${action}'] Added to deluged: \${directoryPath}'\${file}\"
+    if echo \"\${row}\" | grep ' CREATE ' > /dev/null; then
+      delimiter=' CREATE '
+    elif echo \"\${row}\" | grep ' MOVED_TO ' > /dev/null; then
+      delimiter=' MOVED_TO '
+    elif echo \"\${row}\" | grep ' DELETE ' > /dev/null; then
+      delimiter=' DELETE '
+    fi
+    directoryPath=\$(echo \"\${row}\" | sed -E \"s/^(.+?)\${delimiter}(.+?)$/\1/\" | sed -E \"s/(\s)/\\\\\1/g\")
+    action=\$(echo \"\${delimiter}\" | sed -E \"s/\s//g\")
+    fileName=\$(echo \"\${row}\" | sed -E \"s/^(.+?)\${delimiter}(.+?)$/\2/\" | sed -E \"s/(\s)/\\\\\1/g\")
+    fileNameWithoutExtension=\$(echo \"\${fileName}\" | sed -E \"s/^(.+?)\.torrent$/\1/\")
+
+    if [[ \"\${action}\" == 'DELETE' ]]; then
+      activeTorrents=\$(deluge-console --daemon 127.0.0.1 --port 58846 --username deluge --password deluge \"info; exit\")
+      torrentRowToRemove=\$(echo \"\${activeTorrents}\" | grep \"\${fileNameWithoutExtension}\")
+      torrentIdToRemove=\$(echo \"\${torrentRowToRemove}\" | sed -E \"s/^.+?\${fileNameWithoutExtension}\s(.+?)$/\1/\")
+      deluge-console --daemon 127.0.0.1 --port 58846 --username deluge --password deluge \"add \${directoryPath}\${file} --path=\${directoryPath}; exit\"
+      echo \"[\${action}] Removed from deluged: \${directoryPath}\${file}\"
+    else
+      deluge-console --daemon 127.0.0.1 --port 58846 --username deluge --password deluge \"add \${directoryPath}\${file} --path=\${directoryPath}; exit\"
+      echo \"[\${action}] Added to deluged: \${directoryPath}\${file}\"
+    fi
   fi
 done
 "
@@ -60,4 +76,4 @@ sudo systemctl daemon-reload
 sudo systemctl enable deluged-auto-add.service
 
 # Start service now
-sudo systemctl start deluged-auto-add.service
+sudo systemctl restart deluged-auto-add.service
