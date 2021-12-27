a8c371135bfb3b9a02f0c132ed9a2411bca452b3
Romain Fallet <romain@fallet.net>
2021-12-27 22:12:42 +0100

    update

--- scripts/deluge/set-up-auto-add-service.sh
+++ scripts/deluge/set-up-auto-add-service.sh
@@ -35,26 +35,16 @@ while read -r row; do
     fi
     directoryPath=\$(echo \"\${row}\" | sed -E \"s/^(.+?)\${delimiter}(.+?)\$/\1/\")
     directoryPathEscaped=\$(echo \"\${directoryPath}\" | sed -E \"s/(\[|\]|\s|\(|\))/\\\\\\\\\1/g\")
-    echo \"directoryPath: \${directoryPath}\"
-    echo \"directoryPathEscaped: \${directoryPathEscaped}\"
     action=\$(echo \"\${delimiter}\" | sed -E \"s/\s//g\")
-    echo \"action: \${action}\"
     fileName=\$(echo \"\${row}\" | sed -E \"s/^(.+?)\${delimiter}(.+?)\$/\2/\")
     fileNameEscaped=\$(echo \"\${fileName}\" | sed -E \"s/(\[|\]|\s|\(|\))/\\\\\\\\\1/g\")
-    echo \"fileName: \${fileName}\"
-    echo \"fileNameEscaped: \${fileNameEscaped}\"
     fileNameWithoutExtension=\$(echo \"\${fileName}\" | sed -E \"s/^(.+?)\.torrent\$/\1/\")
     fileNameWithoutExtensionEscaped=\$(echo \"\${fileNameWithoutExtension}\" | sed -E \"s/(\[|\]|\s|\(|\))/\\\\\\\\\1/g\")
-    echo \"fileNameWithoutExtension: \${fileNameWithoutExtension}\"
-    echo \"fileNameWithoutExtensionEscaped: \${fileNameWithoutExtensionEscaped}\"
 
     if [[ \"\${action}\" == 'DELETE' ]]; then
       torrentsList=\$(deluge-console --daemon 127.0.0.1 --port 58846 --username deluge --password deluge \"info\")
-      echo \"\${torrentsList}\"
       torrentRowToRemove=\$(echo \"\${torrentsList}\" | grep --fixed-strings \"\${fileNameWithoutExtension}\")
-      echo \"torrentRowToRemove: \${torrentRowToRemove}\"
       torrentIdToRemove=\$(echo \"\${torrentRowToRemove}\" | sed -E \"s/^.+?\s(\w+)\s*$/\1/\")
-      echo \"torrentIdToRemove: \${torrentIdToRemove}\"
       deluge-console --daemon 127.0.0.1 --port 58846 --username deluge --password deluge \"rm -c \${torrentIdToRemove}\"
       echo \"[\${action}] Removed from deluged: \${directoryPath}\${fileName}\"
     else
