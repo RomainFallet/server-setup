cf65fef26d146c125e71ac5422e5f571e748edcb
Romain Fallet <romain@fallet.net>
2022-01-22 14:32:12 +0100

    update

--- scripts/users/create-user.sh
+++ scripts/users/create-user.sh
@@ -12,5 +12,23 @@ then
   read -r -p "Enter the name of the new user: " userName
 fi
 
+# Ask public key
+sshPublicKey=${2}
+if [[ -z "${sshPublicKey}" ]]
+then
+  read -r -p "Enter the public key of the machine that the user will use to log in: " sshPublicKey
+fi
+
+
 # Create user if not existing
 id -u "${userName}" &> /dev/null || sudo useradd --create-home --shell /bin/bash "${userName}"
+
+# Create .ssh directory
+sudo mkdir -p /home/"${userName}"/.ssh
+sudo chmod 0700 ~/.ssh
+
+# Create auhorized keys
+echo "
+${sshPublicKey}" | sudo tee ~/.ssh/authorized_keys > /dev/null
+sudo chmod 0600 ~/.ssh/authorized_keys
+
