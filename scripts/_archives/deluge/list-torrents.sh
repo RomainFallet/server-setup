fdbb4dc9a2d47bfd21ebb9fec4ae3da3ce949a00
Romain Fallet <romain@fallet.net>
2023-05-15 11:19:51 +0200

    chore: remove unused code

--- scripts/_archives/deluge/list-torrents.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Exit script on error
-set -e
-
-### List torrents
-
-# Show infos about torrents
-deluge-console --daemon 127.0.0.1 --port 58846 --username deluge --password deluge "info ; exit"
