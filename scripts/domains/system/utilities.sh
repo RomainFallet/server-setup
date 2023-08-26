eda7ae00c8744e290e5fb8ea607ab24466dd8803
Romain Fallet <romain@fallet.net>
2023-08-26 21:41:11 +0200

    feat: make ipv6 configuration optional

--- scripts/domains/system/utilities.sh
+++ scripts/domains/system/utilities.sh
@@ -44,41 +44,44 @@ Unattended-Upgrade::Automatic-Reboot-Time \"05:00\";"
 }
 
 function SetUpIpv6 () {
-  AskIfNotSet ipv6Address "Enter your IPv6 address (eg. 2001:XXXX:XXX:XXXX::XXXX)"
-  AskIfNotSet ipv6Gateway "Enter your IPv6 gateway (eg. 2001:XXXX:XXX:XXXX::1)"
-  ipv4ConfigurationPath=/etc/netplan/50-cloud-init.yaml
-  ipv6ConfigurationPath=/etc/netplan/51-cloud-init-ipv6.yaml
-  # shellcheck disable=SC2312
-  ethernetConnectionName=$(sudo ls /sys/class/net | sort -V | awk 'FNR == 1 {print}')
-  ethernetConnectionMacAddress=$(cat /sys/class/net/"${ethernetConnectionName}"/address)
-  ipv4Configuration="network:
-    version: 2
-    ethernets:
-        ${ethernetConnectionName}:
-            dhcp4: true
-            match:
-                macaddress: ${ethernetConnectionMacAddress}
-            mtu: 1500
-            set-name: ${ethernetConnectionName}"
-  ipv4ConfigurationPath=/etc/netplan/50-cloud-init.yaml
-  SetFileContent "${ipv4Configuration}" "${ipv4ConfigurationPath}"
-  ipv6Configuration="network:
-    version: 2
-    ethernets:
-        ${ethernetConnectionName}:
-            dhcp6: false
-            mtu: 1500
-            match:
-                macaddress: ${ethernetConnectionMacAddress}
-            set-name: ${ethernetConnectionName}
-            addresses:
-                - ${ipv6Address:?}/128
-            routes:
-                - to: default
-                  via: ${ipv6Gateway:?}
-                - to: ${ipv6Gateway:?}
-                  scope: link"
-  SetFileContent "${ipv6Configuration}" "${ipv6ConfigurationPath}"
-  SetFileContent "network: {config: disabled}" /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
-  EnableNetworkConfiguration
+  Ask configureIpv6 'Configure IPv6? (y/n)' 'y'
+  if [[ "${configureIpv6:?}" == 'y' ]]; then
+    AskIfNotSet ipv6Address "Enter your IPv6 address (eg. 2001:XXXX:XXX:XXXX::XXXX)"
+    AskIfNotSet ipv6Gateway "Enter your IPv6 gateway (eg. 2001:XXXX:XXX:XXXX::1)"
+    ipv4ConfigurationPath=/etc/netplan/50-cloud-init.yaml
+    ipv6ConfigurationPath=/etc/netplan/51-cloud-init-ipv6.yaml
+    # shellcheck disable=SC2312
+    ethernetConnectionName=$(sudo ls /sys/class/net | sort -V | awk 'FNR == 1 {print}')
+    ethernetConnectionMacAddress=$(cat /sys/class/net/"${ethernetConnectionName}"/address)
+    ipv4Configuration="network:
+      version: 2
+      ethernets:
+          ${ethernetConnectionName}:
+              dhcp4: true
+              match:
+                  macaddress: ${ethernetConnectionMacAddress}
+              mtu: 1500
+              set-name: ${ethernetConnectionName}"
+    ipv4ConfigurationPath=/etc/netplan/50-cloud-init.yaml
+    SetFileContent "${ipv4Configuration}" "${ipv4ConfigurationPath}"
+    ipv6Configuration="network:
+      version: 2
+      ethernets:
+          ${ethernetConnectionName}:
+              dhcp6: false
+              mtu: 1500
+              match:
+                  macaddress: ${ethernetConnectionMacAddress}
+              set-name: ${ethernetConnectionName}
+              addresses:
+                  - ${ipv6Address:?}/128
+              routes:
+                  - to: default
+                    via: ${ipv6Gateway:?}
+                  - to: ${ipv6Gateway:?}
+                    scope: link"
+    SetFileContent "${ipv6Configuration}" "${ipv6ConfigurationPath}"
+    SetFileContent "network: {config: disabled}" /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+    EnableNetworkConfiguration
+  fi
 }
