ce6e23c789193f08625d86e575ef213c27b10cdc
Romain Fallet <romain@fallet.net>
2025-03-08 19:31:02 +0100

    feat: remove phoenix application

--- scripts/recipes/application-machine/utilities.sh
+++ scripts/recipes/application-machine/utilities.sh
@@ -13,7 +13,6 @@
 # shellcheck source-path=../../../
 . "${SERVER_SETUP_HOME_PATH:?}/scripts/domains/application/java/index.sh"
 # shellcheck source-path=../../../
-. "${SERVER_SETUP_HOME_PATH:?}/scripts/domains/application/phoenix/index.sh"
 # shellcheck source-path=../../../
 . "${SERVER_SETUP_HOME_PATH:?}/scripts/domains/application/vaultwarden/index.sh"
 # shellcheck source-path=../../../
@@ -27,35 +26,31 @@ function AskApplicationMachineActions () {
   Ask applicationMachineAction "What do you want to do?
   - Nothing............................[0]
   - Set up a Java application..........[1]
-  - Set up a Phoenix application.......[2]
-  - Install or upgrade Forgejo.........[3]
-  - Install or upgrade Mattermost......[4]
-  - Install or upgrade Listmonk........[5]
-  - Install or upgrade Vaultwarden.....[6]
-  - Install or upgrade Drone CI........[7]
-  - Install or upgrade Drone CI Runner.[8]"
+  - Install or upgrade Forgejo.........[2]
+  - Install or upgrade Mattermost......[3]
+  - Install or upgrade Listmonk........[4]
+  - Install or upgrade Vaultwarden.....[5]
+  - Install or upgrade Drone CI........[6]
+  - Install or upgrade Drone CI Runner.[7]"
   if [[ "${applicationMachineAction:?}" == '1' ]]; then
     SetupJavaApplication
   fi
   if [[ "${applicationMachineAction:?}" == '2' ]]; then
-    SetupPhoenixApplication
-  fi
-  if [[ "${applicationMachineAction:?}" == '3' ]]; then
     SetupForgejo
   fi
-  if [[ "${applicationMachineAction:?}" == '4' ]]; then
+  if [[ "${applicationMachineAction:?}" == '3' ]]; then
     SetupMattermost
   fi
-  if [[ "${applicationMachineAction:?}" == '5' ]]; then
+  if [[ "${applicationMachineAction:?}" == '4' ]]; then
     SetupListmonk
   fi
-  if [[ "${applicationMachineAction:?}" == '6' ]]; then
+  if [[ "${applicationMachineAction:?}" == '5' ]]; then
     SetupVaultwarden
   fi
-  if [[ "${applicationMachineAction:?}" == '7' ]]; then
+  if [[ "${applicationMachineAction:?}" == '6' ]]; then
     SetupDroneCi
   fi
-  if [[ "${applicationMachineAction:?}" == '8' ]]; then
+  if [[ "${applicationMachineAction:?}" == '7' ]]; then
     SetupDroneCiRunner
   fi
 }
